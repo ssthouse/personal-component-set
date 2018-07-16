117a00a95fe7e59a1a7c26b053cdb9769e3a4e70
ssthouse <ssthouse@163.com>
2018-07-16 15:33:49 +0800

    Config package.json to public current lib

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import HelloWorld from './HelloWorld.vue'
+const Components = {
+  HelloWorld
+}
+
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name])
+})
+
+export default Components
